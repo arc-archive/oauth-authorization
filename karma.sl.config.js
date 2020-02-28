36e502df5b6dda8eeaba4f75e7f497eb382db5a9
Pawel Psztyc <jarrodek@gmail.com>
2020-02-28 03:14:50 -0800

    test: temporaily removing Safari-1

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -9,7 +9,8 @@ module.exports = (config) => {
       testName: 'oauth-authorization'
     }
   });
-  slConfig.browsers = ['SL_Chrome', 'SL_Chrome-1', 'SL_Firefox', 'SL_Firefox-1', 'SL_Safari', 'SL_Safari-1', 'SL_EDGE'];
+  slConfig.browsers = ['SL_Chrome', 'SL_Chrome-1', 'SL_Firefox', 'SL_Firefox-1', 'SL_Safari', 'SL_EDGE'];
+  // 'SL_Safari-1',
   config.set(merge(createBaseConfig(config), slConfig));
   return config;
 };
