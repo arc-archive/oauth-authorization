c6b0482901e83686bd5d97556aea42862811cf4c
Pawel Psztyc <jarrodek@gmail.com>
2021-01-20 18:24:10 -0800

    chore: updating types
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { OAuth2AuthorizationElement } from './src/OAuth2AuthorizationElement.js';
 export { OAuth1AuthorizationElement } from './src/OAuth1AuthorizationElement.js';
 export { OAuth2Authorization } from './src/OAuth2Authorization.js';
+export { AuthorizationError, CodeError } from './src/AuthorizationError.js';
